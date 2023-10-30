d6ea6895a455af18ae240899ff0b9ca2296456ef
Selim <mamo31st@gmail.com>
2023-10-30 16:05:48 +0200

    update

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates a new node and sets its parent
+ *
+ * @parent: the parent node of the newly created node
+ * @value: value to be stored inside new node
+ *
+ * Return: the newly inserted node (Success), NULL (Failure)
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node = NULL;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node to the left of a parent
+ * binary_tree_insert_right - inserts a node to the right of a parent
  *
  * @parent: the parent node of the inserted node
  * @value: value to be stored inside new node
  *
  * Return: the newly inserted node (Success), NULL (Failure)
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 binary_tree_t *new_node = NULL;
 
@@ -20,13 +20,13 @@ new_node = binary_tree_node(parent, value);
 if (!new_node)
 return (new_node);
 
-if (parent->left)
+if (parent->right)
 {
-new_node->left = parent->left;
-parent->left->parent = new_node;
+new_node->right = parent->right;
+parent->right->parent = new_node;
 }
 
-parent->left = new_node;
+parent->right = new_node;
 
 return (new_node);
 }
